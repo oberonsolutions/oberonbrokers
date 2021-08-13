fd8bdcdf597290d02a57b22a0675b541f3e9211d
Jim Phillips <jim@oberon.group>
2021-08-13 18:09:34 -0400

    Setup initial function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
+const cfgTicker = require('./ticker-config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.updateRates = functions.pubsub.schedule('0 0 * * *')
+  .timeZone('America/New_York')
+  .onRun((context) => {
+    console.log('This will be run every 5 minutes!');
+    return null;
+  });
